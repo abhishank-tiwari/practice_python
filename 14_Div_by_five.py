963aede5755c075da2458a41bbe71a70d1f4b195
abhishank-tiwari <qaer.abhi@gmail.com>
2022-03-17 00:54:06 +0530

    new files 17 march

--- 14_Div_by_five.py
+++ 14_Div_by_five.py
@@ -1,8 +1,8 @@
-nums = { 12 , 16 , 18 , 21 , 26 }
+#Print numbers divisible by 5 using break
+nums = { 12 , 16 , 15 , 21 , 26 }
 for num in nums :
 	if num%5 == 0:
 		print(num)
-		break
+		break          
 else:
 	print("not found")
-
