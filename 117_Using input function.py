2a82b37a0a3bcffe9d2d0d98946dd163d4fdae6f
abhishank-tiwari <qaer.abhi@gmail.com>
2022-04-03 15:30:05 +0530

    New commit 03.04.2022

---
+++ 117_Using input function.py
@@ -0,0 +1,5 @@
+#Using input function
+input("Press enter to begin ! ")
+name = input("Enter name : ")
+age = input("Hello " + name + " What is your age : ")
+print("Hello " + name + " your age is " + age )
