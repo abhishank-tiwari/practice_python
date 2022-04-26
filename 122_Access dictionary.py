7f4d3f071155d2f7945005a7b7b3d0b42fc3cc89
abhishank-tiwari <qaer.abhi@gmail.com>
2022-04-26 07:50:48 +0530

    Program on dictionary

---
+++ 122_Access dictionary.py
@@ -0,0 +1,6 @@
+x = {1:"Ram" , 2:"Shyam" , 3:"Mohan"}
+print(" Dictionary is ",x)
+a = list(x.values())
+b = list(x.keys())
+print("List of keys in dictionary ",b)
+print("List of values in dictionary " , a)
