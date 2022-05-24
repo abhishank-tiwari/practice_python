6d5d490a00aa8fdb02a5189169cda2d7a7b06679
abhishank-tiwari <qaer.abhi@gmail.com>
2022-05-24 07:37:39 +0530

    Check even or odd

---
+++ 154_check number is even or odd.py
@@ -0,0 +1,11 @@
+#Function to check number is even or odd
+def evod(x):
+	if(x%2 == 0):
+		print("Even")
+	else:
+		print("Odd")
+
+print("Checking if 11 is even or odd")
+evod(11)
+print("Checking 12 is even or odd")
+evod(12)
