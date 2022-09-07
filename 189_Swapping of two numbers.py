e92d7382fea37968dc45486be86f3bc575a787c7
abhishank-tiwari <qaer.abhi@gmail.com>
2022-09-07 21:58:44 +0530

    Swapping of two numbers

---
+++ 189_Swapping of two numbers.py
@@ -0,0 +1,10 @@
+a = 2
+b = 3
+print("First number is : ",a)
+print("Second number is : ",b)
+print("Performing Swapping")
+c = a
+a = b
+b = c
+print("After swapping , first number is ",a)
+print("After swapping , second numbe is ",b)
