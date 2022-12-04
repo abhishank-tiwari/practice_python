2a9ed98075038e7fe8572be0cc3646c925d62b2f
abhishank-tiwari <qaer.abhi@gmail.com>
2022-12-04 11:14:29 +0530

    225_current time of system

---
+++ 225_current time of system.py
@@ -0,0 +1,6 @@
+#Print current time of system
+import datetime
+print("Order in which time is printed is : Year , Month , Day , Hour , Minute ,Second and Microsecond")
+#fetching the time in the described format
+x = datetime.datetime.now()
+print(x)
