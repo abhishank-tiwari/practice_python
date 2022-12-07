9702f278cbcda5ed5fb1af03202e10c27480ea16
abhishank-tiwari <qaer.abhi@gmail.com>
2022-12-07 22:33:59 +0530

    Flask program 2

---
+++ 228_Flask program2.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, I am abhishank"
+
+@app.route("/abhi")
+def dello():
+    return "Hello, I am learning data science"
+    
+@app.route("/<name>/")
+def helloname(name):
+    return "Hello  " + name
+    
+app.run(debug = True)
